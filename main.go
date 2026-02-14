c69d06705d211ec5c079828d7a6f5c2e896021d1
Rocha0306 <l.m.p.rocha2005@gmail.com>
2026-02-14 16:39:57 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func diagonalDifference(arr [][]int32) int32 {
+	var elementos_diagnoal_1 []int32
+	var elementos_diagnoal_2 []int32
+	i := 0
+	for j := len(arr) - 1; j >= 0; j-- {
+		elementos_diagnoal_2 = append(elementos_diagnoal_2, arr[j][i])
+		i++
+	}
+
+	i = 0
+	for j := 0; j < len(arr); j++ {
+		if i == j {
+			elementos_diagnoal_1 = append(elementos_diagnoal_1, arr[i][i])
+			i++
+		}
+	}
+
+	var soma_diagonal_1 int32 = 0
+	var soma_diagnoal_2 int32 = 0
+
+	for i := 0; i < len(elementos_diagnoal_1); i++ {
+		soma_diagonal_1 += elementos_diagnoal_1[i]
+		soma_diagnoal_2 += elementos_diagnoal_2[i]
+	}
+
+	return -(soma_diagonal_1 - soma_diagnoal_2)
+
+}
+
+func main() {
+	var array [][]int //array bidimensional
+
+	for i := 0; i < 2; i++ {
+		linha := []int{} // cria uma nova linha
+
+		for j := 0; j < 2; j++ {
+			linha = append(linha, rand.Intn(10)) // adiciona à linha
+		}
+
+		array = append(array, linha) // adiciona a linha completa ao array
+	}
+
+	fmt.Print(diagonalDifference(array))
+}
